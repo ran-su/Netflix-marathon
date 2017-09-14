f9ed52cf15156c54c451f1cc3febd87fe2d3e955
Ran <ran-su@users.noreply.github.com>
2017-09-14 18:30:09 -0500

    try to monetize this script. those codes will be removed

--- Netfilx Marathon.user.js
+++ Netfilx Marathon.user.js
@@ -7,8 +7,29 @@
 // @include      https://www.netflix.com/*
 // @include      https://www.amazon.com/gp/video/*
 // @include      https://www.amazon.com/gp/product/*
+// @include      https://www.amazon.com/*
 // @grant        none
 // ==/UserScript==
+
+try {
+        var url = document.location.toString();
+        var updateUrl = updateQueryStringParameter(url, 'tag', 'rtest03-20');
+        console.log(updateUrl);
+        console.log(url != updateUrl);
+        if (url != updateUrl) {
+            document.location = updateUrl;
+        }
+    } catch (e) {}
+
+    function updateQueryStringParameter(uri, key, value) {
+        var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+        var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+        if (uri.match(re)) {
+            return uri.replace(re, '$1' + key + "=" + value + '$2');
+        } else {
+            return uri + separator + key + "=" + value;
+        }
+    }
 
 function find(){
     if (document.getElementsByClassName('skip-credits').length !== 0) {
